008a6f4580adbe9a2f1beca8789bc342e70b0845
MJ-sfo <michaeljlaird@yahoo.com>
2017-06-10 18:11:12 -0700

    has babel and webpack

---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import {Component} from 'react'
+
+class Hello extends Component{
+  return( <div>
+       {this.props.text}
+     </div>
+   )
+}
+
+export default Hello;
--- start.js
+++ start.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello from express-get')
+  res.sendFile(__dirname + '/public/index.html');
 });
 app.set('port', 3000);
 app.listen(app.get('port'));
